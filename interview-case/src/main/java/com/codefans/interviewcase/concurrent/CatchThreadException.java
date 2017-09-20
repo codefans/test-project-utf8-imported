ff991e2900c3432e18b0e8b7c2bd5d9648328ea1
codefans <363874279@qq.com>
2017-09-20 22:27:56 +0800

    添加通过Thread.setUncaughtExceptionHandler方法捕获线程异常

--- interview-case/src/main/java/com/codefans/interviewcase/concurrent/CatchThreadException.java
+++ interview-case/src/main/java/com/codefans/interviewcase/concurrent/CatchThreadException.java
@@ -8,17 +8,22 @@ import java.util.concurrent.Future;
 /**
  * @Author: ShengzhiCai
  * @Date: 2017-09-14 22:28
+ * 线程异常捕获问题测试类
  */
 
 public class CatchThreadException {
 
     public static void main(String[] args) {
         CatchThreadException cte = new CatchThreadException();
-//        cte.tryCatchThreadExceptionTest();
-        cte.catchThreadExceptionByFutureTest();
+//        cte.cannotCatchThreadExceptionTest();
+        cte.catchThreadExceptionTest();
+//        cte.catchThreadExceptionByFutureTest();
     }
 
-    public void tryCatchThreadExceptionTest() {
+    /**
+     * 正常try-catch捕获不到线程抛出的异常
+     */
+    public void cannotCatchThreadExceptionTest() {
 
         try {
             new Thread(new Runnable() {
@@ -40,11 +45,58 @@ public class CatchThreadException {
 
     }
 
+    /**
+     * 通过Thread.setUncaughtExceptionHandler方法捕获线程抛出的异常
+     */
+    public void catchThreadExceptionTest() {
+        int nThreads = 3;
+        Thread t = null;
+        for(int i = 0; i < nThreads; i ++ ) {
+            try {
+
+
+                final int index = i;
+                t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(index);
+                        throw new RuntimeException("第" + index + "个线程抛异常...");
+                    }
+                });
+
+                t.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+                    @Override
+                    public void uncaughtException(Thread t, Throwable e) {
+                        System.out.println("aaa" + e.getMessage());
+                    }
+                });
+
+                t.start();
+
+
+            } catch (Exception e) {
+                System.out.println("executionEx");
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+
+    }
+
+    /**
+     * 通过Future获取线程异常
+     */
     public void catchThreadExceptionByFutureTest() {
         int nThreads = 3;
         ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
         for(int i = 0; i < nThreads; i ++ ) {
             try {
+
+
+
                 Future future = executorService.submit(new Runnable() {
                     @Override
                     public void run() {
@@ -57,6 +109,9 @@ public class CatchThreadException {
                 System.out.println("exception from thread begin: ");
                 System.out.println(future.get());
                 System.out.println("exception from thread end. ");
+
+
+
             } catch (InterruptedException e) {
                 System.out.println("Interrup....");
                 e.printStackTrace();
