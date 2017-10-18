600b8662c433837307a4aefa00f9c4e5934e713e
codefans <363874279@qq.com>
2017-10-18 17:10:14 +0800

    ArrayList测试类

---
+++ basic-java/src/main/java6/com/codefans/basicjava/util/MyArrayList.java
@@ -0,0 +1,22 @@
+package com.codefans.basicjava.util;
+
+/**
+ * @Author: ShengzhiCai
+ * @Date: 2017-10-07 17:16
+ */
+
+public class MyArrayList {
+
+    private Object[] elementDatas;
+
+    private final int INIT_CAPACITY = 10;
+
+    public MyArrayList() {
+        elementDatas = new Object[INIT_CAPACITY];
+    }
+
+
+
+
+
+}
