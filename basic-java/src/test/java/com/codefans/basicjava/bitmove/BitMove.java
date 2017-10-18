9f3d5bf57783801de6653ac445c07a7e0610e4c2
codefans <363874279@qq.com>
2017-10-18 17:02:30 +0800

    位移操作测试

---
+++ basic-java/src/test/java/com/codefans/basicjava/bitmove/BitMove.java
@@ -0,0 +1,29 @@
+package com.codefans.basicjava.bitmove;
+
+import org.junit.Test;
+
+/**
+ * @Author: ShengzhiCai
+ * @Date: 2017-09-29 17:40
+ */
+
+public class BitMove {
+
+
+    @Test
+    public void bitMoveTest() {
+
+        int m = 11;
+        int n = 2;
+
+        System.out.println("m&n=" + (m&n));
+        System.out.println("m%n=" + (m%n));
+        System.out.println("m/n=" + (m/n));
+
+
+    }
+
+
+
+
+}
