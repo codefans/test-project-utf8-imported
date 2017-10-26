c6086418850f2a86e5491e49923e1b956ddab5a3
caishengzhi <caishengzhi@le.com>
2017-10-26 10:40:48 +0800

    新增一个NIO的例子

---
+++ basic-java/src/main/java6/com/codefans/basicjava/java/nio/net/example02/Server.java
@@ -0,0 +1,25 @@
+package com.codefans.basicjava.java.nio.net.example02;
+
+/**
+ * @author: caishengzhi
+ * @date: 2017-10-25 7:09
+ **/
+public class Server {
+
+    private static int DEFAULT_PORT = 12345;
+    private static ServerHandle serverHandle;
+    public static void start(){
+        start(DEFAULT_PORT);
+    }
+    public static synchronized void start(int port){
+        if(serverHandle!=null) {
+            serverHandle.stop();
+        }
+        serverHandle = new ServerHandle(port);
+        new Thread(serverHandle,"Server").start();
+    }
+    public static void main(String[] args){
+        start();
+    }
+
+}
