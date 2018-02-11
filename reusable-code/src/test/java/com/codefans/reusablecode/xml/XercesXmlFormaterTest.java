94b7913315e1945976076391c4eb114b7c6aaa6b
caishengzhi <caishengzhi@le.com>
2018-02-11 15:00:35 +0800

    dom4j和xerces两种方式格式化xml文件

---
+++ reusable-code/src/test/java/com/codefans/reusablecode/xml/XercesXmlFormaterTest.java
@@ -0,0 +1,20 @@
+package com.codefans.reusablecode.xml;
+
+import org.junit.Test;
+
+/**
+ * @author caishengzhi
+ * @date 2018/2/11 14:49
+ */
+public class XercesXmlFormaterTest {
+
+    @Test
+    public void formatTest() {
+        String xmlSource = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\"><parent><artifactId>test-project-utf8</artifactId><groupId>com.codefans</groupId><version>1.0.0-SNAPSHOT</version></parent><modelVersion>4.0.0</modelVersion><artifactId>reusable-code</artifactId></project>";
+        String formatXml = XercesXmlFormater.format(xmlSource);
+        System.out.println("formatXml:");
+        System.out.println(formatXml);
+
+    }
+
+}
