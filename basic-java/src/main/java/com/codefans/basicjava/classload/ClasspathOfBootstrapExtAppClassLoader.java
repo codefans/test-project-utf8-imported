6a9991ebc9f7a739afa66bbd62ff00baad9e4ab8
mpif <2868679009@qq.com>
2018-04-10 17:00:11 +0800

    打印启动类加载器、扩展类加载器、应用类加载器的加载路径

---
+++ basic-java/src/main/java/com/codefans/basicjava/classload/ClasspathOfBootstrapExtAppClassLoader.java
@@ -0,0 +1,47 @@
+package com.codefans.basicjava.classload;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * @author: codefans
+ * @date: 2018-04-10 16:24
+ */
+public class ClasspathOfBootstrapExtAppClassLoader {
+
+    public static void main(String[] args)
+    {
+        System.out.println("BootstrapClassLoader 的加载路径: ");
+
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for(URL url : urls)
+            System.out.println(url);
+        System.out.println("----------------------------");
+
+        //取得扩展类加载器
+        URLClassLoader extClassLoader = (URLClassLoader)ClassLoader.getSystemClassLoader().getParent();
+
+        System.out.println(extClassLoader);
+        System.out.println("扩展类加载器 的加载路径: ");
+
+        urls = extClassLoader.getURLs();
+        for(URL url : urls)
+            System.out.println(url);
+
+        System.out.println("----------------------------");
+
+
+        //取得应用(系统)类加载器
+        URLClassLoader appClassLoader = (URLClassLoader)ClassLoader.getSystemClassLoader();
+
+        System.out.println(appClassLoader);
+        System.out.println("应用(系统)类加载器 的加载路径: ");
+
+        urls = appClassLoader.getURLs();
+        for(URL url : urls)
+            System.out.println(url);
+
+        System.out.println("----------------------------");
+    }
+
+}
