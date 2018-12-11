a073c7682eb34c2afed1dd900ee7b827a03be77b
mpif <2868679009@qq.com>
2018-12-12 07:39:26 +0800

    新增二叉树操作相关的类

---
+++ interview-case/src/main/java/com/codefans/interview/algorithm/common/BinaryTreeNode.java
@@ -0,0 +1,19 @@
+package com.codefans.interview.algorithm.common;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: codefans
+ * @Date: 2018-12-12 7:35
+ * 二叉树
+ */
+
+public class BinaryTreeNode {
+
+    public int value;
+
+    public BinaryTreeNode left;
+
+    public BinaryTreeNode right;
+
+}
---
+++ interview-case/src/main/java/com/codefans/interview/algorithm/special/ReBuildBinaryTree.java
@@ -0,0 +1,36 @@
+package com.codefans.interview.algorithm.special;
+
+/**
+ * @Author: codefans
+ * @Date: 2018-12-12 7:36
+ * 重建二叉树
+ */
+
+public class ReBuildBinaryTree {
+
+    public static void main(String[] args) {
+        ReBuildBinaryTree reBuildBinaryTree = new ReBuildBinaryTree();
+        reBuildBinaryTree.runCode();
+    }
+
+    public void runCode() {
+        this.initBinaryTree();
+        this.rebuildBinaryTree();
+        this.print();
+    }
+
+    public void initBinaryTree() {
+
+    }
+
+    public void rebuildBinaryTree() {
+
+    }
+
+    public void print() {
+
+    }
+
+
+
+}
