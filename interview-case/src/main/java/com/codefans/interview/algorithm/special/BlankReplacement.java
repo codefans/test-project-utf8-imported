5117f5c9a94c60fc22b9f5d31653310ea7ac3dc7
mpif <caiszf@163.com>
2018-12-12 10:52:47 +0800

    空格替换-两种实现方法 	1.生成新字符串 	2.在源字符串内完成替换

--- interview-case/src/main/java/com/codefans/interview/algorithm/special/BlankReplacement.java
+++ interview-case/src/main/java/com/codefans/interview/algorithm/special/BlankReplacement.java
@@ -11,7 +11,8 @@ public class BlankReplacement {
     public static void main(String[] args) {
         BlankReplacement blankReplacement = new BlankReplacement();
         String originStr = "We are happy.";
-        String resultStr = blankReplacement.replace(originStr);
+//        String resultStr = blankReplacement.replace(originStr);
+        String resultStr = blankReplacement.replaceInOriginStr(originStr);
         System.out.println("resultStr:");
         System.out.println(resultStr);
     }
@@ -27,6 +28,46 @@ public class BlankReplacement {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     *
+     * @param originStr
+     * @return
+     */
+    public String replaceInOriginStr(String originStr) {
+        char[] originChars = originStr.toCharArray();
+        int blankCount = 0;
+        for(int i = 0; i < originChars.length; i ++) {
+            if(originChars[i] == ' ') {
+                blankCount ++;
+            }
+        }
+        //将一个空格字符替换为%20, 相当于多了两个字符
+        char[] destChars = new char[originStr.length() + 2 * blankCount];
+        for(int i = 0; i < originChars.length; i ++) {
+            destChars[i] = originChars[i];
+        }
+
+        int index1 = originStr.length() -1;
+        int index2 = destChars.length - 1;
+        char tmp = ' ';
+        while(index1 > 0 && index2 > 0 && index1 != index2) {
+            if(destChars[index1] != ' ') {
+                tmp = destChars[index2];
+                destChars[index2] = destChars[index1];
+                destChars[index1] = tmp;
+                index1--;
+                index2--;
+            } else {
+                index1--;
+                destChars[index2--] = '0';
+                destChars[index2--] = '2';
+                destChars[index2--] = '%';
+            }
+        }
+
+        return new String(destChars);
     }
 
 }
