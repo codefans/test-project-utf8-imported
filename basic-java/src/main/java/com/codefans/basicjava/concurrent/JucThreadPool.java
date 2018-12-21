bd86b9f65bf249e227d152519fdff45a7f694c59
mpif <caiszf@163.com>
2018-12-21 18:33:12 +0800

    添加JucThreadPool.java

---
+++ basic-java/src/main/java/com/codefans/basicjava/concurrent/JucThreadPool.java
@@ -0,0 +1,17 @@
+package com.codefans.basicjava.concurrent;
+
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @author: codefans
+ * @date: 2018-11-27 15:32
+ */
+public class JucThreadPool {
+
+    public static ExecutorService newExecutorService() {
+        ExecutorService executorService = null;
+
+        return executorService;
+    }
+
+}
