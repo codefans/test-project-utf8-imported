86b637367c50858f6facd907542164bdaa19b115
mpif <caiszf@163.com>
2018-12-21 18:35:24 +0800

    添加synchronized测试类

---
+++ basic-java/src/main/java/com/codefans/basicjava/concurrent/synchronize/SynchronizedBasic.java
@@ -0,0 +1,17 @@
+package com.codefans.basicjava.concurrent.synchronize;
+
+/**
+ * @author: codefans
+ * @date: 2018-12-20 10:02:40
+ *
+ * synchronized
+ * 作用在方法上,锁默认为当前对象；
+ * 作用在静态方法上,锁默认为当前class
+ * 作用在对象上,锁就是这个对象
+ *
+ */
+public class SynchronizedBasic {
+
+
+
+}
